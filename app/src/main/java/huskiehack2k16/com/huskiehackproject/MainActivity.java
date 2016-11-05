7bc31c33b0c16bf91f44b7b3dc92cadbd5b8b2f8
rdaruwala <rdaruwala@gmail.com>
2016-11-05 15:35:38 -0500

    Initial commit

---
+++ app/src/main/java/huskiehack2k16/com/huskiehackproject/MainActivity.java
@@ -0,0 +1,13 @@
+package huskiehack2k16.com.huskiehackproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
