eca6863df6b9a4983e35284950f23a1f42bb2ce6
Eli <eschlossb@gmail.com>
2016-11-05 18:37:12 -0500

    Added empty screens for various pages in app

---
+++ app/build/generated/source/buildConfig/androidTest/debug/huskiehack2k16/com/huskiehackproject/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package huskiehack2k16.com.huskiehackproject.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "huskiehack2k16.com.huskiehackproject.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
